1194b6b151040c9decd9ceeb9cfb3f2bbb04d4c4
Tnze <cjd001113@outlook.com>
2019-10-07 00:32:09 +0800

    添加中文整数的解析函数，由于是照抄C++的代码所以不够优雅

--- zh/num.go
+++ zh/num.go
@@ -1,10 +1,17 @@
 package zh
 
+import (
+	"fmt"
+	"io"
+)
+
+type Uint64 uint64
+
 var nums = [...]string{"零", "一", "二", "三", "四", "五", "六", "七", "八", "九"}
 var secs = [...]string{"", "万", "亿", "万亿", "亿亿"}
 var chns = [...]string{"", "十", "百", "千"}
 
-func String(num uint64) (str string) {
+func (num Uint64) String() (str string) {
 	if num == 0 { // 为零时直接返回
 		return nums[0]
 	}
@@ -34,7 +41,7 @@ func String(num uint64) (str string) {
 	return
 }
 
-func secString(sec uint64) string {
+func secString(sec Uint64) string {
 	var str string
 	var pos int
 	var zero = true
@@ -57,4 +64,50 @@ func secString(sec uint64) string {
 	}
 
 	return str
+}
+
+var pairs = map[rune]struct {
+	value  uint64
+	isUnit bool
+}{
+	'十': {10, false},
+	'百': {100, false},
+	'千': {1000, false},
+	'万': {10000, true},
+	'亿': {100000000, true},
+}
+
+var words = map[rune]uint64{
+	'零': 0, '一': 1, '二': 2, '三': 3, '四': 4,
+	'五': 5, '六': 6, '七': 7, '八': 8, '九': 9,
+}
+
+func (num *Uint64) Scan(state fmt.ScanState, verb rune) error {
+	var number, sec uint64
+	for {
+		v, _, err := state.ReadRune()
+		if err == io.EOF {
+			sec += number
+			*num += Uint64(sec)
+			return nil
+		} else if err != nil {
+			return err
+		}
+		n, ok := words[v]
+
+		if ok {
+			number = n
+		} else {
+			unit := pairs[v]
+			if unit.isUnit {
+				sec = (sec + number) * unit.value
+				*num += Uint64(sec)
+				sec = 0
+			} else {
+				sec += (number * unit.value)
+			}
+
+			number = 0
+		}
+	}
 }
